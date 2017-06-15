d112e5b00626fcc822e3d7cdeea00a8264ad0c57
Raul Alanis <rulosc07@gmail.com>
2017-06-15 13:48:49 -0500

    creacion de CRUD de doctores y pacientes

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -6,7 +6,7 @@ use Yii;
 use yii\web\Controller;
 use yii\web\Response;
 use app\models\EntDoctores;
-
+use app\models\EntPacientes;
 
 class ApiController extends Controller
 {
@@ -36,5 +36,259 @@ class ApiController extends Controller
 
     }
 
-   
+    public function actionCrearDoctor(){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $respuesta['error'] = true;
+        $respuesta['message'] = 'Faltan datos';
+        $doctor =  new EntDoctores();
+
+        if(isset($_REQUEST['nombre']) && isset($_REQUEST['apellido']) && isset($_REQUEST['email']) && isset($_REQUEST['password'])){
+            $doctor->txt_nombre = $_REQUEST['nombre'];
+            $doctor->txt_apellido_paterno = $_REQUEST['apellido'];
+            $doctor->txt_email = $_REQUEST['email'];
+            $doctor->txt_password = $_REQUEST['password'];
+            if($doctor->save()){
+                $respuesta ['error'] = true;
+                $respuesta ['message'] = 'Datos invalidos';
+                $respuesta['errosDoc'] = $doctor->errors;
+            }
+
+            $respuesta ['error'] = false;
+            $respuesta ['message'] = 'Doctor guardado';
+            $respuesta ['doctor'] = $doctor;
+        }else{
+            $respuesta ['error'] = true;
+            $respuesta ['message'] = 'No hay datos';
+        }
+
+        return $respuesta;
+    }
+
+    public function actionLeerDoctor(){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $respuesta['error'] = true;
+        $respuesta['message'] = 'Faltan datos';
+        $doctores = EntDoctores::find()->where(['b_habilitado'=>1])->all();
+
+        if($doctores){
+            $respuesta ['error'] = false;
+            $respuesta ['message'] = 'Doctores mostrados';
+            $respuesta ['doctor'] = $doctores;
+        }else{
+            $respuesta ['error'] = true;
+            $respuesta ['message'] = 'No hay datos';
+        }
+
+        return $respuesta;
+    }
+
+    public function actionActualizarDoctor($idDoctor = 0, $nombre = null, $apellido = null, $email = null, $password = null){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $respuesta['error'] = true;
+        $respuesta['message'] = 'Faltan datos';
+
+        if( (isset($_REQUEST['idDoctor']) && isset($_REQUEST['nombre'])) || (isset($_REQUEST['idDoctor']) && isset($_REQUEST['apellido'])) || 
+        (isset($_REQUEST['idDoctor']) && isset($_REQUEST['email'])) || (isset($_REQUEST['idDoctor']) || isset($_REQUEST['password'])) ){
+            $id = $_REQUEST['idDoctor'];
+            $doctor = EntDoctores::find()->where(['id_doctor'=>$id])->andWhere(['b_habilitado'=>1])->one();           
+
+            if($doctor){
+                if(isset($_REQUEST['nombre'])){
+                    $doctor->txt_nombre = $_REQUEST['nombre'];
+                }
+                if(isset($_REQUEST['apellido'])){
+                    $doctor->txt_apellido_paterno = $_REQUEST['apellido'];
+                }
+                if(isset($_REQUEST['email'])){
+                    $doctor->txt_email = $_REQUEST['email'];
+                }
+                if(isset($_REQUEST['password'])){
+                    $doctor->txt_password = $_REQUEST['password'];
+                }
+
+                if($doctor->save()){
+                    $respuesta ['error'] = false;
+                    $respuesta ['message'] = 'Doctor actualizado';
+                    $respuesta ['doctor'] = $doctor;
+                }else{
+                    $respuesta ['error'] = true;
+                    $respuesta ['message'] = 'Datos invalidos';
+                    $respuesta['errosDoc'] = $doctor->errors;
+                }
+                
+            }else{
+                $respuesta ['error'] = true;
+                $respuesta ['message'] = 'No hay datos';
+            }
+
+        }else{
+            $respuesta ['error'] = true;
+            $respuesta ['message'] = 'No hay datos';
+        }
+
+        return $respuesta;
+    }
+
+    public function actionEliminarDoctor($idDoctor = 0){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $respuesta['error'] = true;
+        $respuesta['message'] = 'Faltan datos';
+
+        if(isset($_REQUEST['idDoctor'])){
+            $id = $_REQUEST['idDoctor'];
+            $doctor = EntDoctores::find()->where(['id_doctor'=>$id])->andWhere(['b_habilitado'=>1])->one();
+
+            if($doctor){
+                $doctor->b_habilitado = 0;
+                if($doctor->save()){
+                    $respuesta ['error'] = false;
+                    $respuesta ['message'] = 'Doctor eliminado';
+                }else{
+                    $respuesta ['error'] = true;
+                    $respuesta ['message'] = 'Datos invalidos';
+                    $respuesta['errosDoc'] = $doctor->errors;
+                }
+            }else{
+                $respuesta ['error'] = true;
+                $respuesta ['message'] = 'No hay datos';
+            }
+
+        }else{
+            $respuesta ['error'] = true;
+            $respuesta ['message'] = 'No hay datos por get';
+        }
+
+        return $respuesta;
+    }
+
+    public function actionCrearPaciente(){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $respuesta['error'] = true;
+        $respuesta['message'] = 'Faltan datos';
+        $paciente = new EntPacientes;
+
+        if(isset($_REQUEST['nombre']) && isset($_REQUEST['apellidoPat']) && isset($_REQUEST['apellidoMat']) && isset($_REQUEST['email']) && isset($_REQUEST['telefono']) && isset($_REQUEST['nacimiento'])){
+            $paciente->txt_nombre = $_REQUEST['nombre'];
+            $paciente->txt_apellido_paterno = $_REQUEST['apellidoPat'];
+            $paciente->txt_apellido_materno = $_REQUEST['apellidoMat'];
+            $paciente->txt_email = $_REQUEST['email'];
+            $paciente->txt_telefono_contacto = $_REQUEST['telefono'];
+            $paciente->fch_nacimiento = $_REQUEST['nacimiento'];
+            if($paciente->save()){
+                $respuesta ['error'] = false;
+                $respuesta ['message'] = 'Paciente guardado';
+                $respuesta ['paciente'] = $paciente;
+            }else{
+                $respuesta ['error'] = true;
+                $respuesta ['message'] = 'Datos invalidos';
+                $respuesta['errosPac'] = $paciente->errors;
+            }
+        }else{
+            $respuesta ['error'] = true;
+            $respuesta ['message'] = 'No hay datos';
+        }
+
+        return $respuesta;
+    }
+
+    public function actionLeerPaciente(){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $respuesta['error'] = true;
+        $respuesta['message'] = 'Faltan datos';
+        $pacientes = EntPacientes::find()->where(['b_habilitado'=>1])->all();
+
+        if($pacientes){
+            $respuesta ['error'] = false;
+            $respuesta ['message'] = 'Doctores mostrados';
+            $respuesta ['doctor'] = $pacientes;
+        }else{
+            $respuesta ['error'] = true;
+            $respuesta ['message'] = 'No hay datos';
+        }
+        
+        return $respuesta;
+    }
+
+    public function actionActualizarPaciente($idPaciente = 0, $nombre = null, $apellidoPat = null, $apellidoMat = null, $email = null, $telefono = null, $nacimiento = null){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $respuesta['error'] = true;
+        $respuesta['message'] = 'Faltan datos';
+
+        if( (isset($_REQUEST['idPaciente']) && isset($_REQUEST['nombre'])) || (isset($_REQUEST['idPaciente']) && isset($_REQUEST['apellidoPat'])) || (isset($_REQUEST['idPaciente']) && isset($_REQUEST['apellidoMat'])) || 
+        (isset($_REQUEST['idPaciente']) && isset($_REQUEST['email'])) || (isset($_REQUEST['idPaciente']) && isset($_REQUEST['telefono'])) || (isset($_REQUEST['idPaciente']) && isset($_REQUEST['nacimiento'])) ){
+            $id = $_REQUEST['idPaciente'];
+            $paciente = EntPacientes::find()->where(['id_paciente'=>$id])->andWhere(['b_habilitado'=>1])->one();           
+
+            if($paciente){
+                if(isset($_REQUEST['nombre'])){
+                    $paciente->txt_nombre = $_REQUEST['nombre'];
+                }
+                if(isset($_REQUEST['apellidoPat'])){
+                    $paciente->txt_apellido_paterno = $_REQUEST['apellidoPat'];
+                }
+                if(isset($_REQUEST['apellidoMat'])){
+                    $paciente->txt_apellido_materno = $_REQUEST['apellidoMat'];
+                }
+                if(isset($_REQUEST['email'])){
+                    $paciente->txt_email = $_REQUEST['email'];
+                }
+                if(isset($_REQUEST['telefono'])){
+                    $paciente->txt_telefono_contacto = $_REQUEST['telefono'];
+                }
+                if(isset($_REQUEST['nacimiento'])){
+                    $paciente->fch_nacimiento = $_REQUEST['nacimiento'];
+                }
+
+                if($paciente->save()){
+                    $respuesta ['error'] = false;
+                    $respuesta ['message'] = 'Paciente actualizado';
+                    $respuesta ['paciente'] = $paciente;
+                }else{
+                    $respuesta ['error'] = true;
+                    $respuesta ['message'] = 'Datos invalidos';
+                    $respuesta['errosPac'] = $paciente->errors;
+                }
+            }else{
+                $respuesta ['error'] = true;
+                $respuesta ['message'] = 'Paciente no encontrado';
+            }  
+        }else{
+            $respuesta ['error'] = true;
+            $respuesta ['message'] = 'No hay datos';
+        }
+
+        return $respuesta;
+    }
+
+    public function actionEliminarPaciente($idPaciente = 0){
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $respuesta['error'] = true;
+        $respuesta['message'] = 'Faltan datos';
+
+        if(isset($_REQUEST['idPaciente'])){
+            $id = $_REQUEST['idPaciente'];
+            $paciente = EntPacientes::find()->where(['id_paciente'=>$id])->andWhere(['b_habilitado'=>1])->one();
+
+            if($paciente){
+                $paciente->b_habilitado = 0;
+                if($paciente->save()){
+                    $respuesta ['error'] = false;
+                    $respuesta ['message'] = 'Doctor eliminado';
+                }else{
+                    $respuesta ['error'] = true;
+                    $respuesta ['message'] = 'Datos invalidos';
+                    $respuesta['errosPac'] = $paciente->errors;
+                }
+            }else{
+                $respuesta ['error'] = true;
+                $respuesta ['message'] = 'No hay datos';
+            }
+
+        }else{
+            $respuesta ['error'] = true;
+            $respuesta ['message'] = 'No hay datos';
+        }
+
+        return $respuesta;
+    }
 }
---
+++ views/api/crearDoctor.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<form>
+    <label>Nombre: </label><input type="text" name="nombre">
+    <label>Apellido: </label><input type="text" name="apellido">
+    <label>Email: </label><input type="text" name="email">
+    <label>Password: </label><input type="password" name="password">
+
+    <button type="submit">Enviar</button>
+</form
