09b4c0dca38b7c317d96292a248c06543c480f31
cloud-elric <humberto@2gom.com.mx>
2017-08-30 12:14:24 -0500

    Se agrego solo texto al envio de correo

---
+++ mail/layouts/recuperarPassText.php
@@ -0,0 +1,8 @@
+Buen día Doctor <?= $nombre . " " . $apellido ?>
+Hemos recibido la solicitud de tu usuario y contraseña.
+Tu información de la cuenta:
+Usuario: <?= $email ?>
+Contraseña: <?= $password ?>
+Este correo se genera automáticamente. 
+Favor de no responder.
+- Saludos de parte del equipo de Springer healthcare.
