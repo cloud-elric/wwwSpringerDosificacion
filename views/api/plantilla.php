9ceb000da90b9e19f6d22f4f73ed45c9482b0c3d
Humberto Antonio Marquez <humberto@2gom.com.mx>
2017-12-27 11:12:40 -0600

    correcion de pdf

--- views/api/plantilla.php
+++ views/api/plantilla.php
@@ -1,212 +1,216 @@
-<?php
-
-use app\models\Utils;
-use yii\helpers\Url;
-
-if (class_exists('yii\debug\Module')) {
-    $this->off(\yii\web\View::EVENT_END_BODY, [\yii\debug\Module::getInstance(), 'renderToolbar']);
-}
-
-$this->registerCssFile(
-    '@web/webAssets/css/plantilla-pdf.css',
-    ['depends' => [\app\assets\AppAsset::className()]]
-);
-?>
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- Estilos-->
-</head>
-
-<body>
-
-    <div class="header">
-        <h1>CONSENTIMIENTO INFORMADO</h1>
-        <p>Para pacientes bajo tratamiento con <b>isotretinoína</b></p>
-    </div>
-
-    <div class="text-right">
-        <p>México
-            <u class="data"><?=$diaNombre?></u> a
-            <u class="data"><?=$diaNumero?></u> de
-            <u class="data"><?=$mes?></u> 20
-            <u class="data"><?=$anio?></u>
-        </p>
-    </div>
-
-    <div>
-        <p>Por medio del presente CONSENTIMIENTO INFORMADO, se autoriza y acepta que el Médico Especialista
-            <u class="data"><?=$nombreDoctor?></u>, con cédula profesional
-            <u class="data"><?=$cedulaDoctor?></u> prescriba a
-            <u class="data"><?=$nombrePaciente?></u> <b>isotretinoína</b>.</p>
-    </div>
-
-    <img class="bullet" src="<?=Url::base()."/webAssets/images/bullet.png"?>" alt=""><h3>Información y Advertencias Importantes</h3>
-    <div class="pos-relative">
-        <p>La isotretinoína puede producir graves malformaciones fetales si se toma, incluso en pequeñas cantidades, durante
-            el embarazo.</p>
-        <p>El riesto de tener un hijo con una malformación grave es extremadamente alto:</p>
-        <ul class="riesgos-bullets">
-            <li><b>Si está embarazada al comenzar este tratamiento</b></li>
-            <li>Si se <b>embaraza</b> durante el tratamiento</li>
-            <li>Si se <b>embaraza</b> 1 mes después de finalizar el tratamiento</li>
-        </ul>
-        <img class="icon-embarazada" src="<?=Url::base()."/webAssets/images/icon-embarazada.png"?>" alt="">
-    </div>
-
-    <img class="bullet" src="<?=Url::base()."/webAssets/images/bullet.png"?>" alt=""><h3>Debe ser llenado y firmado por la paciente, padre o tutor</h3>
-    <div class="contenedor-nota">
-        <p>
-            Lea detenidamente cada uno de los siguientes puntos, y firme este formulario de consentimiento informado, si ha entendido
-            completamente cada uno de los puntos y acepta seguir las instrucciones del especialista. En caso necesario, el
-            padre o tutor debe firmar el formulario de consentimiento después de haberlo leído y entendido en su totalidad.
-            No firme este consentimiento ni comience el tratamiento si hay algo que no entiende sobre la información que
-            ha recibido acerca del uso de este medicamento.
-        </p>
-    </div>
-
-    <div class="secciones-wrapper">
-        <div class="seccion seccion-1">
-            <div class="container-number">
-                <div class="number">
-                    <b>1</b>
-                </div>
-                <p>
-                    Mi médico especialista me ha advertido sobre el riego de graves daños a mi futuro hijo(a) si estoy o voy a quedar embarazada
-                    mientras tomo isotretinoína.
-                </p>
-            </div>
-
-            <div class="container-number">
-                <div class="number">
-                    <b>2</b>
-                </div>
-                <p>
-                    Entiendo que debo esperar hasta el segundo o tercer día de mi próximo periodo menstrual antes de empezar a tomar isotretinoína.
-                </p>
-            </div>
-
-            <div class="container-number">
-                <div class="number">
-                    <b>3</b>
-                </div>
-                <p>
-                    Entiendo que debo contar con una prueba de embarazo negativa antes de empezar mi tratamiento.
-                </p>
-            </div>
-
-            <div class="container-number">
-                <div class="number">
-                    <b>4</b>
-                </div>
-                <p>
-                    He sido informada que debo utilizar métodos de anticonceptivos eficaces y complementarios 1 mes antes de comenzar el tratamiento
-                    durante y <b>1 mes después de
-                    haber finalizado</b>. Deberé utilizar un método fiable de control de natalidad incluso aunque piense
-                    que no puedo quedar embarazada.
-                </p>
-            </div>
-
-            <div class="container-number">
-                <div class="number">
-                    <b>5</b>
-                </div>
-                <p>
-                    Entiendo que dado que todos los métodos anticonceptivos pueden fallar, debo repetir la prueba de embarazo mensualmente durante
-                    el tratamiento y 1 mes posterior a la suspensión del mismo.
-                </p>
-            </div>
-
-        </div>
-        <div class="seccion seccion-2">
-            <div class="container-number">
-                <div class="number">
-                    <b>6</b>
-                </div>
-                <p>
-                    Entiendo que determinados medicamentos pueden disminuir el efecto de los métodos anticonceptivos hormonales. Por ello, informare
-                    a mi médico especialista sobre cualquier medicamento que esté tomando o pretenda tomar, durante el tratamiento
-                    con isotretinoína.
-                </p>
-            </div>
-            <div class="container-number">
-                <div class="number">
-                    <b>7</b>
-                </div>
-                <p>
-                    Si me embarazo durante el tratamiento o al siguiente mes después de haberlo concluido, debo informar inmediatamente de ello
-                    a mi médico especialista.
-                </p>
-            </div>
-            <div class="container-number">
-                <div class="number">
-                    <b>8</b>
-                </div>
-                <p>
-                    Entiendo que por ningún motivo debo dar o compartir mi medicamento con otras personas.
-                </p>
-            </div>
-            <div class="container-number">
-                <div class="number">
-                    <b>9</b>
-                </div>
-                <p>
-                    Entiendo que no debo ingerir bebidas alcohólicas, ya que pueden potencializar los efectos de la isotretinoína y reflejarse
-                    en un incremento de las reacciones adversas.
-                </p>
-            </div>
-        </div>
-    </div>
-
-    <div class="clear"></div>
-    <br>
-    <div class="contenedor-nota text-center">
-        <p>Acepto recibir el tratamiento con isotretinoína.
-            <br>Acepto el riego y las medidas de precaución que debo seguir y que mi médico especialista me han explicado
-            a detalle.<br> Mi médico especialista ha contestado a todas mis preguntas sobre mi tratamiento.</p>
-    </div>
-
-    <div>
-        <div class="seccion seccion-1">
-            <p>Nombre del paciente:</p>
-            <div class="sign-line">
-            <?=$nombrePaciente?>
-            </div>
-
-            <p>Firma del paciente:</p>
-            <div class="sign-line">
-                
-            </div>
-        </div>
-
-        <div class="seccion seccion-2">
-                <p>Nombre del padre/tutor (en caso necesario):</p>
-                <div class="sign-line"></div>
-                <p>Firma del padre/tutor (en caso necesario):</p>
-                <div class="sign-line"></div>
-        </div>
-    </div>
-    <div class="clear"></div>
-
-    <div class="footer-pdf">
-        <div class="bar-solid">
-
-        </div>
-        <div class="logo-footer">
-            <img src="<?=Url::base()."/webAssets/images/logo-italmex.png"?>" alt="">
-        </div>
-    </div>
-    <div class="clear"></div>
-
-</body>
-
-</html>
+<?php	
+	
+	use app\models\Utils;
+	
+	
+	if (class_exists('yii\debug\Module')) {
+	$this->off(\yii\web\View::EVENT_END_BODY, [\yii\debug\Module::getInstance(), 'renderToolbar']);
+	}
+	
+	
+	
+	
+	
+	?>
+	
+	
+	
+	
+	
+	<!DOCTYPE html>
+	<html lang="en">
+	
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		
+		
+		<!-- Estilos-->
+		
+		
+	</head>
+	
+	<body>
+	
+	<div class="header">
+	<h1>CONSENTIMIENTO INFORMADO</h1>
+	<p>Para pacientes bajo tratamiento con <b>isotretinoína</b></p>
+	</div>
+	
+	<div class="text-right">
+	<p>México
+	<u><?=$diaNombre?></u> a
+	<u><?=$diaNumero?></u> de
+	<u><?=$mes?></u> 20
+	<u><?=$anio?></u>
+	</p>
+	</div>
+	
+	<div>
+	<p>Por medio del presente CONSENTIMIENTO INFORMADO, se autoriza y acepta que el Médico Especialista
+	<u><?=$nombreDoctor?></u>, con cédula profesional
+	<u><?=$cedulaDoctor?></u> prescriba a
+	<u><?=$nombrePaciente?></u> <b>isotretinoína</b>.</p>
+	</div>
+	
+	<h3>Información y Advertencias Importantes</h3>
+	<div>
+	<p>La isotretinoína puede producir graves malformaciones fetales si se toma, incluso en pequeñas cantidades, durante
+	el embarazo.</p>
+	<p>El riesto de tener un hijo con una malformación grave es extremadamente alto:</p>
+	<ul>
+	<li><b>Si está embarazada al comenzar este tratamiento</b></li>
+	<li>Si se <b>embaraza</b> durante el tratamiento</li>
+	<li>Si se <b>embaraza</b> 1 mes después de finalizar el tratamiento</li>
+	</ul>
+	
+	</div>
+	
+	<h3>Debe ser llenado y firmado por la paciente, padre o tutor</h3>
+	<div class="contenedor-nota">
+	<p>
+	Lea detenidamente cada uno de los siguientes puntos, y firme este formulario de consentimiento informado, si ha entendido
+	completamente cada uno de los puntos y acepta seguir las instrucciones del especialista. En caso necesario, el
+	padre o tutor debe firmar el formulario de consentimiento después de haberlo leído y entendido en su totalidad.
+	No firme este consentimiento ni comience el tratamiento si hay algo que no entiende sobre la información que
+	ha recibido acerca del uso de este medicamento.
+	</p>
+	</div>
+	
+	<div>
+	<div class="seccion seccion-1">
+	<div class="container-number">
+	<div class="number">
+	<b>1</b>
+	</div>
+	<p>
+	Mi médico especialista me ha advertido sobre el riego de graves daños a mi futuro hijo(a) si estoy o voy a quedar embarazada
+	mientras tomo isotretinoína.
+	</p>
+	</div>
+	
+	<div class="container-number">
+	<div class="number">
+	<b>2</b>
+	</div>
+	<p>
+	Entiendo que debo esperar hasta el segundo o tercer día de mi próximo periodo menstrual antes de empezar a tomar isotretinoína.
+	</p>
+	</div>
+	
+	<div class="container-number">
+	<div class="number">
+	<b>3</b>
+	</div>
+	<p>
+	Entiendo que debo contar con una prueba de embarazo negativa antes de empezar mi tratamiento.
+	</p>
+	</div>
+	
+	<div class="container-number">
+	<div class="number">
+	<b>4</b>
+	</div>
+	<p>
+	He sido informado que debo utilizar métodos de anticonceptivos eficaces y complementarios 1 mes antes de comenzar el tratamiento
+	durante y <b>1 mes después de
+	haber finalizado</b>. Deberé utilizar un método fiable de control de natalidad incluso aunque piense
+	que no puedo quedar embarazada.
+	</p>
+	</div>
+	
+	<div class="container-number">
+	<div class="number">
+	<b>5</b>
+	</div>
+	<p>
+	Entiendo que dado que todos los métodos anticonceptivos pueden fallar, debo repetir la prueba de embarazo mensualmente durante
+	el tratamiento y 1 mes posterior a la suspensión del mismo.
+	</p>
+	</div>
+	
+	</div>
+	<div class="seccion seccion-2">
+	<div class="container-number">
+	<div class="number">
+	<b>6</b>
+	</div>
+	<p>
+	Entiendo que determinados medicamentos pueden disminuir el efecto de los métodos anticonceptivos hormonales. Por ello, informare
+	a mi médico especialista sobre cualquier medicamento que esté tomando o pretenda tomar, durante el tratamiento
+	con isotretinoína.
+	</p>
+	</div>
+	<div class="container-number">
+	<div class="number">
+	<b>7</b>
+	</div>
+	<p>
+	Si me embarazo durante el tratamiento o al siguiente mes después de haberlo concluido, debo informar inmediatamente de ello
+	a mi médico especialista.
+	</p>
+	</div>
+	<div class="container-number">
+	<div class="number">
+	<b>8</b>
+	</div>
+	<p>
+	Entiendo que por ningún motivo debo dar o compartir mi medicamento con otras personas.
+	</p>
+	</div>
+	<div class="container-number">
+	<div class="number">
+	<b>9</b>
+	</div>
+	<p>
+	Entiendo que no debo ingerir bebidas alcohólicas, ya que pueden potencializar los efectos de la isotretinoína y reflejarse
+	en un incremento de las reacciones adversas.
+	</p>
+	</div>
+	</div>
+	</div>
+	
+	<div class="clear"></div>
+	<br>
+	<div class="contenedor-nota text-center">
+	<p>Acepto recibir el tratamiento con isotretinoína.
+	<br>Acepto el riego y las medidas de precaución que debo seguir y que mi médico especialista me han explicado
+	a detalle.<br> Mi médico especialista ha contestado a todas mis preguntas sobre mi tratamiento.</p>
+	</div>
+	
+	<div>
+	<div class="seccion seccion-1">
+	<p>Nombre del paciente:</p>
+	<div class="sign-line">
+	<?=$nombrePaciente?>
+	</div>
+	
+	<p>Firma del paciente:</p>
+	<div class="sign-line">
+	
+	</div>
+	</div>
+	
+	<div class="seccion seccion-2">
+	<p>Nombre del padre/tutor (en caso necesario):</p>
+	<div class="sign-line"></div>
+	 <p>Nombre del padre/tutor (en caso necesario):</p>
+	<div class="sign-line"></div>
+	</div>
+	</div>
+	<div class="clear"></div>
+	
+	 <div class="footer">
+	<div class="bar-solid">
+	
+	</div>
+	<div class="logo-footer">
+	
+	</div>
+	</div>
+	<div class="clear"></div>
+	
+	</body>
+	
+	</html>
